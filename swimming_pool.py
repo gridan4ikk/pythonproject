26a5bfecf892af5372094907175a5070abaca0db
gridan4ikk <111232289+gridan4ikk@users.noreply.github.com>
2023-04-20 20:43:16 +0300

    pythonproject

---
+++ swimming_pool.py
@@ -0,0 +1,87 @@
+# Initialize variables
+pool_volume = int(input("Enter pool volume in gallons: "))
+filter_capacity = int(input("Enter filter capacity in gallons: "))
+current_water_level = 0
+
+# Display menu options
+menu = {
+    "B": "Backwash",
+    "R": "Rinse",
+    "F": "Filter",
+    "W": "Waste",
+    "C": "Recirculate",
+    "X": "Closed"
+}
+
+
+# Define functions for each option
+def backwash():
+    global current_water_level
+    print("Activating backwash valve...")
+    print("Draining wastewater from the pool...")
+    current_water_level -= filter_capacity
+    print("Current water level: {} gallons".format(current_water_level))
+
+
+def rinse():
+    print("Activating rinse valve...")
+    print("Running pump for a few minutes...")
+    print("Current water level: {} gallons".format(current_water_level))
+
+
+def pool_filter():
+    print("Activating filter valve...")
+    print("Cleaning the filter...")
+    print("Maintaining current water level...")
+    print("Current water level: {} gallons".format(current_water_level))
+
+
+def waste():
+    global current_water_level
+    print("Activating waste valve...")
+    print("Draining water from the pool without passing through the filter...")
+    current_water_level -= int(input("Enter amount of water to drain in gallons: "))
+    print("Current water level: {} gallons".format(current_water_level))
+
+
+def recirculate():
+    print("Activating recirculate valve...")
+    print("Bypassing the filter and circulating water back into the pool...")
+    print("Current water level: {} gallons".format(current_water_level))
+
+
+def closed():
+    print("Closing all valves and turning off the pump...")
+
+
+# Main program loop
+while True:
+    # Display menu options
+    print("Menu:")
+    for key, value in menu.items():
+        print("{}: {}".format(key, value))
+    option = input("Enter option: ").upper()
+
+    # Perform action based on option
+    if option == "B":
+        backwash()
+    elif option == "R":
+        rinse()
+    elif option == "F":
+        pool_filter()
+    elif option == "W":
+        waste()
+    elif option == "C":
+        recirculate()
+    elif option == "X":
+        closed()
+        break
+    else:
+        print("Invalid option. Please try again.")
+
+    # Ask if user wants to perform another action
+    another_action = input("Perform another action? (Y/N): ").upper()
+    if another_action == "N":
+        break
+
+print("Program ended.")
