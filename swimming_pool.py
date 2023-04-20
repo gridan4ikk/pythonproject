aa87ae4f875074b1db44d66501f5e90d258d641e
gridan4ikk <111232289+gridan4ikk@users.noreply.github.com>
2023-04-20 22:46:04 +0300

    pythonproject

--- swimming_pool.py
+++ swimming_pool.py
@@ -1,6 +1,6 @@
 # Initialize variables
-pool_volume = int(input("Enter pool volume in gallons: "))
-filter_capacity = int(input("Enter filter capacity in gallons: "))
+pool_volume = int(input("Enter pool volume in liters: "))
+filter_capacity = int(input("Enter filter capacity in liters: "))
 current_water_level = 0
 
 # Display menu options
@@ -20,34 +20,34 @@ def backwash():
     print("Activating backwash valve...")
     print("Draining wastewater from the pool...")
     current_water_level -= filter_capacity
-    print("Current water level: {} gallons".format(current_water_level))
+    print("Current water level: {} liters".format(current_water_level))
 
 
 def rinse():
     print("Activating rinse valve...")
     print("Running pump for a few minutes...")
-    print("Current water level: {} gallons".format(current_water_level))
+    print("Current water level: {} liters".format(current_water_level))
 
 
 def pool_filter():
     print("Activating filter valve...")
     print("Cleaning the filter...")
     print("Maintaining current water level...")
-    print("Current water level: {} gallons".format(current_water_level))
+    print("Current water level: {} liters".format(current_water_level))
 
 
 def waste():
     global current_water_level
     print("Activating waste valve...")
     print("Draining water from the pool without passing through the filter...")
-    current_water_level -= int(input("Enter amount of water to drain in gallons: "))
-    print("Current water level: {} gallons".format(current_water_level))
+    current_water_level -= int(input("Enter amount of water to drain in liters: "))
+    print("Current water level: {} liters".format(current_water_level))
 
 
 def recirculate():
     print("Activating recirculate valve...")
     print("Bypassing the filter and circulating water back into the pool...")
-    print("Current water level: {} gallons".format(current_water_level))
+    print("Current water level: {} liters".format(current_water_level))
 
 
 def closed():
